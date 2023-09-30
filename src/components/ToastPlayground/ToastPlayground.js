98a48ce3c03009ea4de790d0b6fd76ee4339a46c
joe <joestpierre1994@gmail.com>
2023-09-29 20:09:43 -0400

    Initial commit

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React from "react";
 
-import Button from '../Button';
+import Button from "../Button";
+import ToastShelf from "../ToastShelf";
 
-import styles from './ToastPlayground.module.css';
+import styles from "./ToastPlayground.module.css";
 
-const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
+const VARIANT_OPTIONS = ["notice", "warning", "success", "error"];
 
 function ToastPlayground() {
+  const [toasts, setToasts] = React.useState([]);
+  const [toastMessage, setToastMessage] = React.useState("");
+  const [toastVariant, setToastVariant] = React.useState(VARIANT_OPTIONS[0]);
+
+  function handleNewToast(event) {
+    event.preventDefault();
+    const newToast = {
+      id: crypto.randomUUID(),
+      toastMessage,
+      toastVariant,
+    };
+
+    setToasts([...toasts, newToast]);
+    setToastMessage("");
+    setToastVariant(VARIANT_OPTIONS[0]);
+  }
+
+  function handleDismiss(id) {
+    const filteredToast = toasts.filter((toast) => toast.id !== id);
+    setToasts(filteredToast);
+  }
+
   return (
     <div className={styles.wrapper}>
       <header>
@@ -14,48 +37,55 @@ function ToastPlayground() {
         <h1>Toast Playground</h1>
       </header>
 
-      <div className={styles.controlsWrapper}>
+      <ToastShelf toasts={toasts} handleDismiss={handleDismiss} />
+
+      <form className={styles.controlsWrapper} onSubmit={handleNewToast}>
         <div className={styles.row}>
           <label
             htmlFor="message"
             className={styles.label}
-            style={{ alignSelf: 'baseline' }}
+            style={{ alignSelf: "baseline" }}
           >
             Message
           </label>
           <div className={styles.inputWrapper}>
-            <textarea id="message" className={styles.messageInput} />
+            <textarea
+              required
+              id="message"
+              className={styles.messageInput}
+              value={toastMessage}
+              onChange={(event) => setToastMessage(event.target.value)}
+            />
           </div>
         </div>
 
         <div className={styles.row}>
           <div className={styles.label}>Variant</div>
-          <div
-            className={`${styles.inputWrapper} ${styles.radioWrapper}`}
-          >
-            <label htmlFor="variant-notice">
-              <input
-                id="variant-notice"
-                type="radio"
-                name="variant"
-                value="notice"
-              />
-              notice
-            </label>
-
-            {/* TODO Other Variant radio buttons here */}
+          <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
+            {VARIANT_OPTIONS.map((option) => (
+              <label htmlFor={option} key={option}>
+                <input
+                  required
+                  id={option}
+                  type="radio"
+                  name="variant"
+                  value={option}
+                  checked={option === toastVariant}
+                  onChange={(event) => setToastVariant(event.target.value)}
+                />
+                {option}
+              </label>
+            ))}
           </div>
         </div>
 
         <div className={styles.row}>
           <div className={styles.label} />
-          <div
-            className={`${styles.inputWrapper} ${styles.radioWrapper}`}
-          >
+          <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
             <Button>Pop Toast!</Button>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
---
+++ src/components/ToastProvider/ToastProvider.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const toastContext = React.createContext();
+
+function ToastProvider({ children }) {
+  return <div></div>;
+}
+
+export default ToastProvider;
